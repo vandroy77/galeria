c567ffef6a3e85ce25b4cca8f257ceebd067ed1d
iago <iagometroid77@gmail.com>
2022-01-11 21:10:47 -0300

    projeto galeria nasceu!

---
+++ backend/src/main/java/com/dv/dsgallery/DsgalleryApplication.java
@@ -0,0 +1,13 @@
+package com.dv.dsgallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsgalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsgalleryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dv/dsgallery/DsgalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dv.dsgallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsgalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
