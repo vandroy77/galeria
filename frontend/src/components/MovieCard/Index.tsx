9f4411c076343816a6c9d0a1d2ab5446d2fce217
iago <iagometroid77@gmail.com>
2022-01-13 00:09:44 -0300

    navigation buttons

--- frontend/src/components/MovieCard/Index.tsx
+++ frontend/src/components/MovieCard/Index.tsx
@@ -1,6 +1,7 @@
 import MovieScore from "components/MovieScore"
+import { Link } from "react-router-dom";
 
-function MoveCard() {
+function MovieCard() {
     const movie = {
         id: 1,
         image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
@@ -8,18 +9,21 @@ function MoveCard() {
         count: 2,
         score: 4.5
     };
-    
+
     return (
         <div>
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
                 <MovieScore />
-                <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                <Link to={`/form/${movie.id}` }>
+                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+
+                </Link>
             </div>
         </div>
     )
 };
 
 
-export default MoveCard
+export default MovieCard
